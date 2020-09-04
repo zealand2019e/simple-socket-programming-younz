7991a11c3fe682a163f06eb77e1e1f11bf6124ca
younzable <youn0079@edu.ucl.dk>
2020-09-04 10:27:42 +0200

    new class made and start of the local server made

---
+++ EchoServer/Sever.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+   public static class Sever
+    {
+        public static void Start()
+        {
+            TcpListener socketServer = new TcpListener(IPAddress.Loopback, port: 7777); // forbundet med port 7777
+            
+            socketServer.Start();
+            // start sever
+
+            // accept client
+            TcpClient connectionSocket = socketServer.AcceptTcpClient();
+            Console.WriteLine("server activated");
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+        }
+    }
+}
