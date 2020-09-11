a8996bfc697a785163685c8ca2e3b29114941661
younzable <youn0079@edu.ucl.dk>
2020-09-11 11:13:02 +0200

    new project added and new class added to the project

---
+++ EchoClient/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public static void start()
+        {
+            TcpClient socketClient = new TcpClient(hostname:"localhost",port: 7777);
+            Stream ns = socketClient.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            
+            sw.AutoFlush = true;
+
+            Console.WriteLine("indtast besked");
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+
+        }
+    }
+}
---
+++ EchoClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
